3c7082b3c4433af105e1413110720147db8c572c
henzyd <henzydee@gmail.com>
2025-07-22 12:03:02 +0100

    fix(ui): input

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -4,18 +4,23 @@ import { cn } from "~/lib/utils";
 export interface InputProps extends React.ComponentProps<"input"> {
   startAdornment?: React.ReactNode;
   endAdornment?: React.ReactNode;
+  wrapperClassName?: string;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, startAdornment, endAdornment, ...props }, ref) => {
+  ({ className, startAdornment, endAdornment, wrapperClassName, ...props }, ref) => {
     return (
-      <div className="flex items-center gap-4">
+      <div
+        className={cn(
+          "dark:bg-input/30 border-input focus-within:border-primary focus-within:ring-ring/50 flex items-center gap-4 rounded-md border bg-transparent px-4 py-2 focus-within:ring-[1px]",
+          wrapperClassName
+        )}
+      >
         {startAdornment}
         <input
           data-slot="input"
           className={cn(
-            "file:text-foreground placeholder:text-black/80 selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+            "file:text-foreground selection:bg-primary selection:text-primary-foreground flex w-full text-base transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent placeholder:text-black/40 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
             "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
             className
           )}
