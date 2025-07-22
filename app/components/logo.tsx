e6f0da08d13961364058d736e6fcbbe829f1a708
henzyd <henzydee@gmail.com>
2025-07-22 06:42:09 +0100

    updated logo

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -6,7 +6,10 @@ type Props = React.ComponentProps<"h5"> & {
 
 export default function Logo({ className, ...props }: Props) {
   return (
-    <h5 {...props} className={cn("text-2xl font-bold text-nowrap", className)}>
+    <h5
+      {...props}
+      className={cn("text-2xl font-semibold italic text-nowrap", className)}
+    >
       Resolve It
     </h5>
   );
