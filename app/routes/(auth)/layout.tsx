66eba081e74111a4febe182f2287258abf0435eb
henzyd <henzydee@gmail.com>
2025-07-22 06:52:18 +0100

    updated auth layout

--- app/routes/(auth)/layout.tsx
+++ app/routes/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import Logo from "~/components/logo";
 
 export default function AuthLayout() {
   return (
-    <div className="bg-primary-50 flex min-h-screen w-full items-center justify-center p-4">
+    <div className="bg-zinc-100 flex min-h-screen w-full items-center justify-center p-4">
       <main className="largeMobile_545:px-4 flex w-full max-w-[500px] flex-col gap-8 rounded-2xl bg-white p-6 shadow-xs">
         <header className="flex justify-center gap-4 pt-2">
           <Link to={"/"}>
